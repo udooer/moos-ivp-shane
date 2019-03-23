cf00ebf00b4f7c32c38c266a3b8fbc431b2b3910
shane <hunghsuyong114shane@gmail.com>
2019-03-23 19:20:07 +0800

    pattern survey BHV_Polygon

---
+++ missions/behavior2/shell_script/cmd_Line_ARG.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for ARGI in $@;do 
+    echo $ARGI
+done
--- src/lib_behaviors-shane/BHV_Polygon.h
+++ src/lib_behaviors-shane/BHV_Polygon.h
@@ -1,20 +1,21 @@
 /************************************************************/
 /*    NAME: shane                                              */
 /*    ORGN: MIT                                             */
-/*    FILE: BHV_ZigLeg.h                                      */
+/*    FILE: BHV_Polygon.h                                      */
 /*    DATE:                                                 */
 /************************************************************/
 
-#ifndef ZigLeg_HEADER
-#define ZigLeg_HEADER
+#ifndef Polygon_HEADER
+#define Polygon_HEADER
 
 #include <string>
 #include "IvPBehavior.h"
+#include "XYPoint.h"
 
-class BHV_ZigLeg : public IvPBehavior {
+class BHV_Polygon : public IvPBehavior {
 public:
-  BHV_ZigLeg(IvPDomain);
-  ~BHV_ZigLeg() {};
+  BHV_Polygon(IvPDomain);
+  ~BHV_Polygon() {};
   
   bool         setParam(std::string, std::string);
   void         onSetParamComplete();
@@ -27,22 +28,31 @@ public:
   IvPFunction* onRunState();
 
 protected: // Local Utility functions
+  void decode();
+  void postViewPoint();
+  std::vector<double> getDiscretex();
+  std::vector<double> getDiscretey();
   IvPFunction* buildFunctionWithZAIC();
-
 protected: // Configuration parameters
-  double m_zig_angle;
-  double m_zig_duration;
-  double m_priority_wt;
-  double m_zig_time;
-  double m_local_time;
-  
-  double m_WPT_index;
-  double m_last_index;
+  std::string m_path;
+  std::string m_polygon;
+  double m_center_x;
+  double m_center_y;
+  double m_radius;
 
-  int m_case;
-  int m_count;
-  double m_heading;
-  
+  double m_start_x;
+  double m_start_y;
+  double m_omega;
+  double m_amplitude;
+  double m_desired_speed;
+  double m_arrival_radius;
+
+  char m_char;
+  XYPoint point;
+  std::vector<double> x_point;
+  std::vector<double> y_point;
+  int m_index;
+
 protected: // State variables
   double m_osx;
   double m_osy;
@@ -52,6 +62,6 @@ protected: // State variables
 
 extern "C" {
   IVP_EXPORT_FUNCTION IvPBehavior * createBehavior(std::string name, IvPDomain domain) 
-  {return new BHV_ZigLeg(domain);}
+  {return new BHV_Polygon(domain);}
 }
 #endif
